f6ff1ae8c7d429ae1de6c3e5c659278b046ae92b
alanacalvo <alana.keeton@gmail.com>
2022-07-23 00:27:00 -0400

    put route completed

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -2,43 +2,50 @@ const express = require('express');
 const app = express.Router();
 const Session = require('../models/session-model');
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   Session.find({})
     .then(sessions => res.send(sessions))
     .catch(console.error)
 });
 
-app.get('/:id', (req,res) => {
+app.get('/:id', (req, res) => {
+  console.log('fuck', req.params)
   Session.findById(req.params.id)
-    .populate('userId')
+    // .populate('userId')
+    // .then(console.log('userId'))
     .then(session => res.send(session))
     .catch(console.error)
 })
 
-// app.put('/finish/:id', (req,res) => {
-//   Session.findByIdAndUpdate(req.params.id, 
-//     {todos: req.body.todos, notes: req.body.notes})
-//     .then(session => res.send(session))
-//     .catch(console.error)
-// })
+app.put('/:id', (req, res) => {
+  Session.findByIdAndUpdate(req.params.id,
+    {
+      postSessionTodos: req.body.postSessionTodos,
+      notes: req.body.notes,
+      colorRating: req.body.colorRating,
+      starRating: req.body.starRating
+    })
+    .then(session => res.send(session))
+    .catch(console.error)
+})
 
 
 // WILL NEED ANOTHER .POST FOR POST SESSION SURVEYS
 
-app.post('/newsession',(req,res) => {
+app.post('/newsession', (req, res) => {
   Session.create(req.body)
     .then(sessions => {
       res.send(sessions)
     })
-  .catch(console.error)
+    .catch(console.error)
 })
 
-app.delete('/:id', (req,res) => {
-  Session.findOneAndRemove({_id:req.params.id})
-  .then(sessions => {
-    res.send(sessions)
-  })
-  .catch(console.error)
+app.delete('/:id', (req, res) => {
+  Session.findOneAndRemove({ _id: req.params.id })
+    .then(sessions => {
+      res.send(sessions)
+    })
+    .catch(console.error)
 })
 
 module.exports = app;
