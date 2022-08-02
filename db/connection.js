eca102dfbcc881808d46917f7d5092b3de8aac95
alanacalvo <alana.keeton@gmail.com>
2022-08-02 13:31:50 -0400

    fixing .env issue

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-// require('dotenv').config();
+require('dotenv').config();
 
 const mongoURI = 
 process.env.NODE_ENV === 'production'
 ? process.env.DB_URL
-: 'mongodb+srv://alanacalvo:Gajsjdbfu1@cluster0.mzihy.mongodb.net/adhdhub?retryWrites=true&w=majority'
+: process.env.DEV_DB_URL
 
 mongoose.connect(mongoURI)
     .then(instance => console.log(`connected to: ${instance.connections[0].name}`))
